df7cdc137f148fadc52f397c38a76d23b504bccc
DESKTOP-3ED7E68\user <konnitiwantanmen>
2023-09-11 09:57:59 +0900

    playerすくリプと追加

---
+++ GameJam2/Assets/nojikousei/VerticalCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalCamera : MonoBehaviour
+{
+    float anglespeed = 1;
+    Vector3 angle;
+    public float anglestop = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Verticalangle();
+    }
+    void Verticalangle()
+    {
+        angle += new Vector3(Input.GetAxisRaw("Mouse Y"),0 , 0);
+        transform.localEulerAngles= -angle;
+        if (angle.x >= anglestop)
+        {
+            angle.x = anglestop;
+        }
+        if (angle.x <= -anglestop)
+        {
+            angle.x = -anglestop;
+        }
+    }
+}
