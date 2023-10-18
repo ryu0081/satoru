e9b4612cc59ae7a00141371a84e20a4306c3e690
nkcdaichan <dai.ru.kuro.1230@gmail.com>
2023-10-18 15:06:08 +0900

    AI制作中

--- GameJam2/Assets/EnemycpuManager.cs
+++ GameJam2/Assets/EnemycpuManager.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemycpuManager : MonoBehaviour
 {
+    public enum EnemyAiState
+    {
+        Idle,        //‘Ò‹@
+        Walk,        //œpœj
+        Chase,       //’ÇÕ
+        Leave,       //“¦‘–
+        Alert,       //Œx‰ú
+        Freeze,      //d’¼
+        Slashattack, //‹ßÚUŒ‚
+        Laserbeam,   //‰“‹——£UŒ‚1
+        Laserbullet, //‰“‹——£UŒ‚2
+    }
+    public EnemyAiState Enemystate =EnemyAiState.Idle;
+    public int tactic = 1;  //ó‹µ‚Ì”»•Ê—pƒpƒ‰ƒ[ƒ^
+
+
+
+    void setAI()
+    {
+
+    }
     // Start is called before the first frame update
     void Start()
     {
